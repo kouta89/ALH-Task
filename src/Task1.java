768db869bc7458822add993ccb38ba94f2ac4e45
kouta89 <yamamotojb89@gmail.com>
2023-05-26 09:17:20 +0900

    [add]task1,task2

--- src/Task1.java
+++ src/Task1.java
@@ -1,9 +1,11 @@
 public class Task1{
 	public static void main(String[] args){
+		
+		//最終的な合計値を記録する変数
 		int number = 0;
+		
 		for(int i = 1;i <= 100;i++) {
 			number +=i;
-			//System.out.println(i);
 		}
 		System.out.println(number);
 	}
--- src/Task2.java
+++ src/Task2.java
@@ -1,11 +1,12 @@
 public class Task2{
 	public static void main(String[] args){
 		
+		//最終的な合計値を記録する変数
 		int number = 0;
+		
 		for(int i = 2;i <= 100;i++) {
 			if(i%2==0) {
 				number +=i;
-				//System.out.println(i);
 			}
 		}
 		System.out.println(number);
